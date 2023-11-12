2b3a0fda58d66911f3e9ae9ebe17f3ee339639c0
Ben Rahman <dacatbeamgithub@gmail.com>
2023-11-12 11:04:03 -0600

    Add Some Logging

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use portfolio_site::run;
 use std::net::{SocketAddr, Ipv4Addr, IpAddr};
+use std::process;
 
 #[tokio::main]
 async fn main() {
@@ -7,7 +8,10 @@ async fn main() {
     let port = 3000;
     let socket_addr = SocketAddr::new(IpAddr::V4(host), port);
 
+    println!("Starting server at: {}", socket_addr);
+
     if let Err(msg) = run(socket_addr).await {
         eprintln!("Error: {:?}", msg);
+        process::exit(1);
     }
 }
